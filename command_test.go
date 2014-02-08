9a4b3ca43e49ea2bae6ae0e6d35563401cf06557
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-02-08 22:51:41 +0100

    debug travis

--- command_test.go
+++ command_test.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"testing"
 )
 import . "launchpad.net/gocheck"
@@ -14,10 +15,19 @@ var _ = Suite(&CommandSuite{})
 
 func (self *CommandSuite) TestSetDefaultsBasic(c *C) {
 	cmd := NewCommand("./test", "-exit=0", "-time=1")
-	cmd.Run()
-	cmd.Wait()
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	response := cmd.GetResponse()
+	fmt.Println(response)
 
 	c.Assert(response.Failed, Equals, false)
 	c.Assert(response.ExitCode, Equals, 0)
