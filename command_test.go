d5fc3744ebff5008f79f600826dcd8f530d5ba0d
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-02-08 23:17:53 +0100

    basic test

--- command_test.go
+++ command_test.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 import . "launchpad.net/gocheck"
 
@@ -13,22 +14,64 @@ type CommandSuite struct{}
 
 var _ = Suite(&CommandSuite{})
 
-func (self *CommandSuite) TestSetDefaultsBasic(c *C) {
+func (self *CommandSuite) TestBasic(c *C) {
 	cmd := NewCommand("./test", "-exit=0", "-time=1")
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if err := cmd.Wait(); err != nil {
-		fmt.Println(err)
-		return
-	}
+	cmd.Run()
+	cmd.Wait()
 
 	response := cmd.GetResponse()
-	fmt.Println(response)
 
 	c.Assert(response.Failed, Equals, false)
 	c.Assert(response.ExitCode, Equals, 0)
+	c.Assert(response.Stdout, HasLen, 588895)
+	c.Assert(response.Stderr, HasLen, 0)
+	c.Assert(response.Pid, Not(Equals), 0)
+	c.Assert(int(response.Elapsed/time.Second), Aprox, 1, 0.10)
+}
+
+var Aprox Checker = &aproxChecker{&CheckerInfo{
+	Name: "Aprox",
+	Params: []string{
+		"Value",
+		"Aproximate value",
+		"Margin",
+	},
+}}
+
+type aproxChecker struct {
+	*CheckerInfo
+}
+
+func (c *aproxChecker) Check(params []interface{}, names []string) (result bool, error string) {
+	var value, match, margin float64
+
+	if param, ok := params[0].(int); !ok {
+		return false, "First parameter is not an int"
+	} else {
+		value = float64(param)
+	}
+
+	if param, ok := params[1].(int); !ok {
+		return false, "Second parameter is not an int"
+	} else {
+		match = float64(param)
+	}
+
+	if param, ok := params[2].(float64); !ok {
+		return false, "Third parameter is not an float64"
+	} else {
+		margin = float64(param)
+	}
+
+	lowBound := match - (match * margin)
+	if value < lowBound {
+		return false, fmt.Sprintf("Lower than bound %f", lowBound)
+	}
+
+	highBound := match + (match * margin)
+	if value > highBound {
+		return false, fmt.Sprintf("Higher than bound %f", highBound)
+	}
+
+	return true, ""
 }
